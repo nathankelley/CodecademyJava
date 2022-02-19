f455aa63e557df8c312e5c9ce400b6bebfcfd316
Nathan Kelley <59635186+nathankelley@users.noreply.github.com>
2022-02-18 18:35:06 -0800

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,16 @@
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] args){
+    // Introduction, print tree to screen using asteriks
+    System.out.println("My name is Nathan and this a Java program I've created on Codecademy.");
+    System.out.println("The purpose of this program is to print a tree using asteriks to the screen.");
+
+    // tree below
+    System.out.println("   *  ");
+    System.out.println("  ***  ");
+    System.out.println(" ***** ");
+    System.out.println("*******");
+    System.out.println("   *  ");
+    System.out.println("/\\/\\/\\");
+  }
+}
